f6add949612833dce73e21b28aa387ea500f765f
Kafur Kanlıyelek <kafurkanliyelek@transay.local>
2024-10-10 10:11:47 +0300

    nodemon inited, get req

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo name',
+    description: 'Photo description',
+  };
+  res.send(photo);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('Sunucu', port, 'portunda başladı');
+});
